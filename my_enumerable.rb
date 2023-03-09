07f99bd5baf6b709f40eb1ceaa7c5739148e5059
Rashed Arman <rashedarman21@gmail.com>
2023-03-10 02:43:30 +0600

    fix: resolve linter errors

--- my_enumerable.rb
+++ my_enumerable.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
+# This module is used to create a custom enumerable
 module MyEnumerable
   def all?
     nums = []
--- my_list.rb
+++ my_list.rb
@@ -1,12 +1,15 @@
+# frozen_string_literal: true
+
 require './my_enumerable'
 
+# MyList class
 class MyList
   include MyEnumerable
 
   attr_reader :list
 
   def initialize(*list)
-    @list = list 
+    @list = list
   end
 
   def each
@@ -14,16 +17,15 @@ class MyList
   end
 end
 
-
 list = MyList.new(1, 2, 3, 4)
 
 # Test all?
-list.all? {|e| e < 5} # true
-list.all? {|e| e > 5} # false
+list.all? { |e| e < 5 } # true
+list.all? { |e| e > 5 } # false
 
 # Test any?
-list.any? {|e| e == 2} # true
-list.any? {|e| e == 5} # false
+list.any? { |e| e == 2 } # true
+list.any? { |e| e == 5 } # false
 
 # Test filter
-list.filter? {|e| e.even?} # [2, 4]
+list.filter(&:even?) # [2, 4]
